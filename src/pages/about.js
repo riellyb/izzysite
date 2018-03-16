f2ffdedffacd56ffcaa7785f734a314db7c83bd6
Brendan Rielly <brielly@backcountry.com>
2018-03-16 16:34:30 -0600

    initial commit

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const AboutPage = () => (
+  <div>
+    <h1>About me</h1>
+    <p>This is my about me page</p>
+    <p>Check it out</p>
+    <Link to="/">Home</Link>
+  </div>
+)
+
+export default AboutPage
---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Izzy Washburn</h1>
+    <p>This is my portfolio</p>
+    <p>Check it out</p>
+    <Link to="/about/">About Me</Link>
+  </div>
+)
+
+export default IndexPage
