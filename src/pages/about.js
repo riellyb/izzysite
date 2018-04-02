895ac602520508e39fba17ed223ceea1ca88f377
Brendan Rielly <brendanrielly@gmail.com>
2018-04-01 21:34:39 -0600

    Fixed about

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,7 +4,7 @@ import Link from 'gatsby-link'
 const AboutPage = () => (
   <div className="slide-inner">
     <h1>about</h1>
-    <p>izzy Washburn is a designer and illustrator living and working in Salt Lake City. She is currently attending the University of Utah design program.</p>
+    <p>Izzy Washburn is a designer and illustrator living and working in Salt Lake City. She is currently attending the University of Utah design program.</p>
     <p>Her hobbies include eating, baking, watching TV, and bursting into song. She also enjoys birds, puns, and dogs.</p>
   </div>
 )
