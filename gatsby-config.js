ec0c0eab3c0c540f13f63f4bd3add7b2b4217cff
Brendan Rielly <brielly@backcountry.com>
2018-04-04 14:55:38 -0600

    started favicon work

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,26 @@
 module.exports = {
-  siteMetadata: {
-    title: 'Izzy Washburn',
-  },
-  plugins: ['gatsby-plugin-react-helmet'],
+    siteMetadata: {
+        title: 'Izzy Washburn',
+    },
+    plugins: [
+         'gatsby-plugin-react-helmet'//,
+        // {
+        //     resolve: `gatsby-plugin-favicon`,
+        //     options: {
+        //         logo: "./src/favicon.png",
+        //         injectHTML: true,
+        //         icons: {
+        //             android: true,
+        //             appleIcon: true,
+        //             appleStartup: true,
+        //             coast: false,
+        //             favicons: true,
+        //             firefox: true,
+        //             twitter: false,
+        //             yandex: false,
+        //             windows: false
+        //         }
+        //     }
+        // }
+    ],
 };
