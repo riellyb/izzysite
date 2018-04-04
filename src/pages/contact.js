842741e17a220fa5835e266c3e576b251d943cb2
Brendan Rielly <brendanrielly@gmail.com>
2018-04-03 22:30:17 -0600

    udpdated about ingo

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,7 +5,7 @@ const ContactPage = () => (
   <div className="slide-inner">
     <h1>contact</h1>
     <p><a href="mailto:iz.b.washburn@gmail.com">iz.b.washburn@gmail.com</a></p>
-    <p>@izzywashburn</p>
+    <p><a href="https://www.instagram.com/izwashburn/">@izwashburn</a></p>
     <p><a href="http://www.behance.net/izbwashbur3e76">behance.net/izbwashbur3e76</a></p>
     <p><a href="mailto:iz.b.washburn@gmail.com">Email me</a> for my resume, or if you would like a PDF or printed copy of my current portfolio.</p>
   </div>
