31f27a648e4df21daded1da8156ff1fd64a5fd91
Brendan Rielly <brielly@backcountry.com>
2018-08-07 11:42:57 -0400

    changed slc to boston on about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,7 +4,7 @@ import Link from 'gatsby-link'
 const AboutPage = () => (
   <div className="slide-inner" style={{maxWidth: '550px'}}>
     <h1>about</h1>
-    <p>Izzy Washburn is a designer and illustrator living and working in Salt Lake City. She is a graduate of the University of Utah Graphic Design program.</p>
+    <p>Izzy Washburn is a designer and illustrator living and working in Boston. She is a graduate of the University of Utah Graphic Design program.</p>
     <p>Her hobbies include eating, baking, watching TV, and bursting into song. She also enjoys birds, puns, and dogs.</p>
   </div>
 )
